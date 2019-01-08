f3ee74622c5e5d10535a2aceb49bd2ad1e378782
Lucas Dahl <LucasDahl425@yahoo.com>
2019-01-08 12:26:14 -0800

    Added the container for the user input using code.

--- ChattyDemo/Controller/LoginController.swift
+++ ChattyDemo/Controller/LoginController.swift
@@ -9,12 +9,59 @@
 import UIKit
 
 class LoginController: UIViewController {
+    
+    //================================
+    //MARK: Properties for UI elements
+    //================================
+    
+    
+    // Input container
+    var inputsContainerView: UIView = {
+        
+        // Setup the UI elements
+        let view = UIView()
+        
+        // Set the background color
+        view.backgroundColor = UIColor.white
+        
+        // Allow
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Round the corners
+        view.layer.cornerRadius = 5
+        // This is needed or the corners won't be rounded.
+        view.layer.masksToBounds = true
+        
+        // Return the view
+        return view
+        
+    }()
+    
+    // button
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Change the background color
         view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
+        
+        // Add the container to the subView
+        view.addSubview(inputsContainerView)
+        
+        // Call the container setup function
+        setupInputContainerView()
+        
+    }
+    
+    
+    func setupInputContainerView() {
+        
+        // Add the constraints
+        inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
         
     }
     
@@ -25,7 +72,7 @@ class LoginController: UIViewController {
     }
 
 
-}
+}// End class
 
 extension UIColor {
     
