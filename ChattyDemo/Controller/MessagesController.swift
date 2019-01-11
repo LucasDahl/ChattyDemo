b3862530df23ccf381785f1f06225098ccd8d047
Lucas Dahl <LucasDahl425@yahoo.com>
2019-01-11 06:29:33 -0800

    The current user's name is now displayed in the tableview when logged in.

--- ChattyDemo/Controller/MessagesController.swift
+++ ChattyDemo/Controller/MessagesController.swift
@@ -18,16 +18,43 @@ class MessagesController: UITableViewController {
         // Make the left bar button item(Logout)
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
         
+        // Check for user being logged in
+        checkIfUserIsLoggedIn()
+        
+    }
+
+    // Check if the user is logged in
+    func checkIfUserIsLoggedIn() {
+        
         // Check if the user a logged in or not
         if Auth.auth().currentUser?.uid == nil {
             
             // User is not logged in and present the controller after a slight delay
             perform(#selector(handleLogout), with: nil, afterDelay: 0)
             
+        } else {
+            
+            // Get the UID
+            let uid = Auth.auth().currentUser?.uid
+            Database.database().reference().child("users").child(uid!).observeSingleEvent(of: .value, with: { (snapshot) in
+                
+                //print(snapshot) // printe user email and name
+                
+                // Get data out of the snapshot dictionary
+                if let dictionary = snapshot.value as? [String: AnyObject] {
+                    
+                    // Get the users name to display on the navigation bar
+                    self.navigationItem.title = dictionary["name"] as? String
+                    
+                }
+                
+                
+                
+            }, withCancel: nil) // Use this method if you are having issues with the complier
+            
         }
-        
-    }
 
+    }
 
     // Logout function
     @objc func handleLogout() {
