e6a4e4c009c31cccd7cc30cfea65d57c20a71378
Lucas Dahl <LucasDahl425@yahoo.com>
2019-01-14 07:05:58 -0800

    The users profile image is now displayed in the tableview, and a default image is given if the user has not selected one.

--- ChattyDemo/Controller/NewMessageController.swift
+++ ChattyDemo/Controller/NewMessageController.swift
@@ -54,6 +54,11 @@ class NewMessageController: UITableViewController {
         }, withCancel: nil)
     }
     
+    // Sets the row height for the tableview
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 72
+    }
+    
     @objc func handleCancel() {
         
         dismiss(animated: true, completion: nil)
@@ -70,7 +75,7 @@ class NewMessageController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         // Make the cell
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! UserCell
         
         // Get the user
         let user = users[indexPath.row]
@@ -81,6 +86,11 @@ class NewMessageController: UITableViewController {
         // Set the users email to the dertail label
         cell.detailTextLabel?.text = user.email
         
+        // Get the image url for the user
+        if let profileImageUrl = user.profileImageUrl {
+            cell.profileImageView.loadImageUsingCacheWithUrlString(profileImageUrl)
+        }
+        
         return cell
         
     }
@@ -90,8 +100,51 @@ class NewMessageController: UITableViewController {
 // Create a custom cell
 class UserCell: UITableViewCell {
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        textLabel?.frame = CGRect(x: 64, y: textLabel!.frame.origin.y - 2, width: textLabel!.frame.width, height: textLabel!.frame.height)
+        
+        detailTextLabel?.frame = CGRect(x: 64, y: detailTextLabel!.frame.origin.y + 2, width: detailTextLabel!.frame.width, height: detailTextLabel!.frame.height)
+    }
+    
+    // Default imageview
+    let profileImageView: UIImageView = {
+        
+        // Setup the imageview
+        let imageView = UIImageView()
+        
+        // This is needed to use the constraints
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Set the corner radius
+        imageView.layer.cornerRadius = 24
+        
+        // Mask to bounds
+        imageView.layer.masksToBounds = true
+        
+        // Set the aspect fit
+        imageView.contentMode = .scaleAspectFill
+        
+        // Defualt image
+        imageView.image = UIImage(named: "gameofthrones_splash")
+        
+        return imageView
+        
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        
+        // Add subview
+        addSubview(profileImageView)
+        
+        // Constraints for the imageview
+        profileImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
+        profileImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 48).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -99,5 +152,3 @@ class UserCell: UITableViewCell {
     }
     
 }
-
-
